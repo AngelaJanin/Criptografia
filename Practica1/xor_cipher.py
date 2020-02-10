17c6fe2e52c86bc5b5e36d7b6f97c8365243a027
ailynrp12 <ailynrp12@ciencias.unam.mx>
2020-02-10 09:28:52 -0600

    xor listo

--- Practica1/xor_cipher.py
+++ Practica1/xor_cipher.py
@@ -5,7 +5,41 @@ def cipher(message):
     Parámetro:
        message -- el mensaje a cifrar.
     """
-    pass
+   cadena_binario = ""
+    letras_nums = []
+    xor = []
+    cifrado = ""
+    # Pasamos cada letra del mensaje a una cadena binaria y
+    # guardamos su valor en binario en una lista para no perderlos
+    for letra in message:
+        aux = aBinario(letra)
+        cadena_binario = cadena_binario + aux
+        letras_nums.append(aux)
+ 
+    # Le aplicamos a cada elemento de la lista la funcion XOR
+    for elemento in letras_nums:
+        palabra_binaria = ""
+        longitud = len(elemento)
+        c = 0
+        # Aplicamos XOR para cada bit de la letra 
+        for i in range(longitud):
+            if c == longitud-1:
+                if elemento[c] != format(1, 'b'):
+                    palabra_binaria = palabra_binaria + "1"
+                else:
+                    palabra_binaria = palabra_binaria + "0"
+            else:
+                palabra_binaria = palabra_binaria + elemento[c]
+            c = c + 1 
+        xor.append(palabra_binaria)
+
+    cifrado = ""
+    # Concatenamos cada letra cifrada con XOR 
+    for elem in xor:
+        entero = aEntero(elem)
+        palabra_cifrada = aLetra(entero)
+        cifrado = cifrado + palabra_cifrada
+    return cifrado
 
 def decipher(criptotext):
     """
@@ -14,4 +48,59 @@ def decipher(criptotext):
     Parámetro:
        cryptotext -- el mensaje a descifrar.
     """
-    pass
+   cadena_binario = ""
+    letras_nums = []
+    xor = []
+    texto_claro = ""
+    # Pasamos cada letra del criptotexto a una cadena binaria y
+    # guardamos su valor en binario en una lista para no perderlos
+    for criptoletra in criptotext:
+        aux = aBinario(criptoletra)
+        cadena_binario = cadena_binario + aux
+        letras_nums.append(aux)
+ 
+    # Le aplicamos a cada elemento la función inversa de XOR
+    for elemento in letras_nums:
+        palabra_binaria = ""
+        longitud = len(elemento)
+        c = 0
+        # Ahora encontramos los 1 para cambiarlos por 0
+        for i in range(longitud):
+            if c == longitud-1:
+                if elemento[c] == format(1, 'b'):
+                    palabra_binaria = palabra_binaria + "0"
+                else:
+                    palabra_binaria = palabra_binaria + "1"
+            else:
+                palabra_binaria = palabra_binaria + elemento[c]
+            c = c + 1 
+        xor.append(palabra_binaria)
+
+    cifrado = ""
+    # Concatenamos cada letra en claro
+    for elem in xor:
+        entero = aEntero(elem)
+        palabra_clara = aLetra(entero)
+        texto_claro = texto_claro + palabra_clara
+    return texto_claro
+
+"""
+Función auxiliar que convierte una letra a su número binario
+correspondiente 
+"""
+def aBinario(letra):
+    return format(ord(letra), 'b')
+
+"""
+Función auxiliar que convierte un número binario a un 
+número entero 
+"""
+def aEntero(numero):
+    return int(numero , 2)
+
+"""
+Función auxiliar que convierte un número entero a su 
+letra o caracter correspondiente 
+"""
+def aLetra(numero):
+    return chr(numero)
