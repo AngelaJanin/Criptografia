510f05b467d9f33e9739ee1b210a604cfc648ce6
Ailyn Rebollar <ailynrp12@ciencias.unam.mx>
2020-03-05 15:39:51 -0600

    Pequeño avance de cifrado de Vigenere

--- Practica2/vigenere.py
+++ Practica2/vigenere.py
@@ -10,6 +10,8 @@ class Vigenere():
         :param alphabet: Alfabeto a trabajar con el cifrado.
         :param password: El password que puede ser o no dada por el usuario.
         """
+        self.alphabet = alphabet
+        self.password = password if password else self.construyePass()
 
     def cipher(self, message):
         """
@@ -18,7 +20,14 @@ class Vigenere():
         :param message: El mensaje a cifrar.
         :return: Una cadena de texto con el mensaje cifrado.
         """
+        aux = self.completaCadena(message)
+        cifrado = ""
+        for i in range(len(aux)):
+            index_aux = self.alphabet.find(aux[i])
+            index_mes = self.alphabet.find([message[i]])
+            index = index_aux + index_mes % len(self.alphabet)
 
+                  
 
     def decipher(self, ciphered):
         """
@@ -26,3 +35,34 @@ class Vigenere():
         :param ciphered: El criptotexto a decifrar.
         :return: El texto plano correspondiente del parámetro recibido.
         """
+
+    def construyePass(self):
+        return ''.join(random.choice(self.alphabet) for i in range(4,10))
+
+    """
+    Función auxiliar que acompleta la cadena que recibe de acuerdo a la longitud del 
+    password
+    """
+    def completaCadena(self, cadena):
+        long_m = len(cadena)
+        long_p = len(self.password)
+        diferencia = long_m - long_p
+        if diferencia == 0:
+            return self.password
+        else:
+            if long_m < long_p:
+                return self.password[:long_m]
+            else:
+                n, r = divmod(long_m, long_p)
+                return self.password * n + self.password[:r]
+
+
+
+alphabet = "ABCDEFGHIJKLMNÑOPQRSTUVWXYZ"
+message = "ES"
+short = "PASS"
+long = "ENDURECIDAMENTE"
+semi_long = "ENDURECIMIENTO"
+vig_short = Vigenere(alphabet, short)
+print(vig_short.cipher(message))
+"TSMWTSNFBEFLPJWUENG"
